28576ad9cdb6ed551f90ec3bceb95283987fb7f6
Serhii-Leonenko <leonenko3149@gmail.com>
2024-12-12 11:39:42 +0200

    added pagination

--- messenger/views.py
+++ messenger/views.py
@@ -3,6 +3,7 @@ from django.template.context_processors import request
 from rest_framework import generics, status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
